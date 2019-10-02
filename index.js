8933947ab397b7fdfb417403f2266058ca619538
Matt Huberty <matthewhuberty77@gmail.com>
2019-10-02 10:36:30 -0500

    This commit is too big. Shoulda commited sooner

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const express = require('express')
 const app = express()
 const beerRouter = require('./routes/beerRouter')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/beers', beerRouter)
 
 app.use('/', (req, res) => {
     res.send(`<h1>Welcome to the beer API</h1>`)
+})
+
+
+mongoose.connect('mongodb://localhost:27017/beers', {
+    useNewUrlParser: true
+})
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to the beers database!`)
+})
+mongoose.connection.on('error', (err) => {
+    console.log(`Error connecting to beers database:\n${err}`)
 })
 
 const port  = process.env.PORT || 4444
--- routes/beerRouter.js
+++ routes/beerRouter.js
@@ -1,12 +1,54 @@
 const express = require('express')
 const beerRouter = express.Router()
+const Beer = require('../models/beer')
 
 beerRouter.get('/', (req, res) => {
-    // send beers
+    Beer.find((err, beers) => {
+        if (err) {
+            res.status(400);
+            res.send(err);
+        } else {
+            res.json(beers);
+        }
+    })
+ });
+ 
+
+beerRouter.get('/:beer_id', (req, res) => {
+    Beer.findById(req.params.beer_id, (err, beer) => {
+        if (err) {
+            res.status(400).send(err)
+        } else {
+            res.json(beer)
+        }
+    })
 })
 
 beerRouter.post('/', (req, res) => {
-    // save a beer
+    let beer = new Beer()
+    beer.name = req.body.name
+    beer.rating = req.body.rating
+    beer.save((err, document) => {
+        if (err) {
+            console.log(`Error saving beer:\n${err}`)
+            res.status(400)
+            res.send(err)
+        } else {
+            res.send(`Saved your ${document}`)
+        }
+    })
+})
+
+beerRouter.delete('/:beer_id', (req, res) => {
+    Beer.deleteOne({
+        _id: req.params.beer_id
+    }, (err) => {
+        if (err) {
+            res.status(400).send(err)
+        } else {
+            res.status(200).send(`Successfully deleted beer with id: ${req.params.beer_id}`)
+        }
+    })
 })
 
 beerRouter.use('/', (req, res) => {
